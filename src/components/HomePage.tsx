213ebf614800ea54f4e8d955a85188a8ca62ca18
lucasmargni04 <lucasmargni@gmail.com>
2025-12-07 19:24:33 -0300

    Instalation

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+    return (
+        <div className="text-blue-900 text-9xl">
+            Home
+        </div>
+    );
+};
+
+export default HomePage;
