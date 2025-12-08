972c576222954ca22314020c6f4485585aecc550
lucasmargni04 <lucasmargni@gmail.com>
2025-12-07 21:53:40 -0300

    Trying HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
-        <div className="text-blue-900 text-9xl">
-            Home
+        <div className="min-h-screen min-w-screen bg-gray-100 p-30 flex flex-col items-center">
+            <Link to="/snake" className="w-5/8 p-6 bg-white rounded-xl shadow-2xl border-4 border-blue-500 cursor-pointer text-center">
+                <div className="text-blue-900 text-left pl-20">
+                    <h2 className="text-4xl font-bold">
+                        Snake
+                    </h2>
+                </div>
+            </Link>
+
         </div>
     );
 };
